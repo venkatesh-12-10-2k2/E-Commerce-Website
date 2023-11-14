a761ede4ebbc5da0e249683766979f9799b5faba
venkatesh-12-10-2k2 <sudhasudha97276@gmail.com>
2023-11-14 15:06:24 +0530

    Updated CORS policy

--- backend/functions/server.js
+++ backend/functions/server.js
@@ -11,10 +11,16 @@ const userRoute = require("../routes/UserRoutes");
 const newProduct = require("../routes/ProductRoutes");
 const paymentRoute = require("../routes/PaymentRoutes");
 
+const corsOptions = {
+	origin: '*',
+	credentials: true,            //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+}
+
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 
 //port
